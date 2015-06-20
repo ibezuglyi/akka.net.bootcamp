a24d94f0a32875f3438aff9fcd4dd3a9008205b3
Ievgen Bezuglyi <ievgen@revio.no>
2015-06-20 14:05:25 +0200

    unit 1 lesson 4

--- unit1/Program.cs
+++ unit1/Program.cs
@@ -14,15 +14,22 @@ namespace WinTail
 
             Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
             IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
-            Props validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
-            IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
-            Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+
+            Props tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            IActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
+
+            Props fileValidatorActorProps = Props.Create(() => new FileValidatorActor(consoleWriterActor, tailCoordinatorActor));
+            IActorRef fileValidatorActor = MyActorSystem.ActorOf(fileValidatorActorProps, "validationActor");
+
+            Props consoleReaderProps = Props.Create<ConsoleReaderActor>(fileValidatorActor);
             IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
-            
+
+            // begin processing
             consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             MyActorSystem.AwaitTermination();
         }
+
     }
     #endregion
 }
